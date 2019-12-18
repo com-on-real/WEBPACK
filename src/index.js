c0cb5393d19b4bef6a4a7bdab62370fa79670c54
Florian BROTTE <f.brotte@comon-real.fr>
2019-12-18 18:16:16 +0100

    Manifest + Clean + Optimize css sass + correction filename

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
-import './style/style.sass';
-console.log('salut');
+console.log('caca');
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,74 @@
-const HTMLPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dev = process.env.NODE_ENV === 'development'
+const path = require('path')
 
-module.exports = {
-	plugins: [
-		new HTMLPlugin({
-			template: 'dist/index.html',
-		}),
-	  new MiniCssExtractPlugin({
-		filename: 'style.css',
-	  }),
-	],
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DashboardPlugin = require("webpack-dashboard/plugin")
+
+let cssLoaders = [
+	{loader: 'css-loader', options: {importLoaders: 1}}
+]
+
+if (!dev) {
+	cssLoaders.push({
+		loader: 'postcss-loader',
+		options: {
+			plugins: (loader) => [
+				require('autoprefixer')({
+					browers: ['last 2 versions', 'ie > 8']
+				})
+			]
+		}
+	})
+}
+
+let config = {
+	entry: {
+		app: ['./src/style/style.sass', './src/index.js']
+	},
+	output: {
+		path: path.resolve('./public'),
+		filename: dev ? '[name].js' : '[name].[chunkhash:4].js',
+		publicPath: '/public/'
+	},
 	module: {
 	  rules: [
+		{
+			test: /\.css$/,
+			use: cssLoaders
+		},
 		{
 			test: /\.(sa|sc|c)ss$/,
 			use: [
 			  {
 				loader: MiniCssExtractPlugin.loader,
 				options: {
-				publicPath: 'dist/',
+				publicPath: 'public/assets/css/',
 				  hmr: process.env.NODE_ENV === 'development',
 				},
 			  },
-			  'css-loader',
-			  'postcss-loader',
-			  'sass-loader',
+			  ...cssLoaders, 'sass-loader'
 			],
 		},
 	  ],
 	},
-  };
+	plugins: [
+		new MiniCssExtractPlugin({
+		  filename: dev ? '[name].css' : '[name].[contenthash:4].css',
+		  chunkFilename: '[id].css',
+		  disable: dev,
+		}),
+		new DashboardPlugin(),
+		new CleanWebpackPlugin({
+			verbose: true
+		})
+	  ]
+}
+
+  if (!dev) {
+	config.plugins.push(new ManifestPlugin())
+	console.log('Version de production')
+}
+
+module.exports = config
